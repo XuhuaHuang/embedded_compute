5ecff88d555bcc8c90ed329dd3d50e8f5469fe36
Template Janitor <template.janitor@example.com>
2026-05-31 21:27:21 +0000

    Cleanup template and initialize repository

--- ProjectOptions.cmake
+++ ProjectOptions.cmake
@@ -6,7 +6,7 @@ include(CheckCXXCompilerFlag)
 include(CheckCXXSourceCompiles)
 
 
-macro(myproject_supports_sanitizers)
+macro(embedded_compute_supports_sanitizers)
   # Emscripten doesn't support sanitizers
   if(EMSCRIPTEN)
     set(SUPPORTS_UBSAN OFF)
@@ -57,110 +57,110 @@ macro(myproject_supports_sanitizers)
   endif()
 endmacro()
 
-macro(myproject_setup_options)
-  option(myproject_ENABLE_HARDENING "Enable hardening" ON)
-  option(myproject_ENABLE_COVERAGE "Enable coverage reporting" OFF)
+macro(embedded_compute_setup_options)
+  option(embedded_compute_ENABLE_HARDENING "Enable hardening" ON)
+  option(embedded_compute_ENABLE_COVERAGE "Enable coverage reporting" OFF)
   cmake_dependent_option(
-    myproject_ENABLE_GLOBAL_HARDENING
+    embedded_compute_ENABLE_GLOBAL_HARDENING
     "Attempt to push hardening options to built dependencies"
     ON
-    myproject_ENABLE_HARDENING
+    embedded_compute_ENABLE_HARDENING
     OFF)
 
-  myproject_supports_sanitizers()
+  embedded_compute_supports_sanitizers()
 
-  if(NOT PROJECT_IS_TOP_LEVEL OR myproject_PACKAGING_MAINTAINER_MODE)
-    option(myproject_ENABLE_IPO "Enable IPO/LTO" OFF)
-    option(myproject_WARNINGS_AS_ERRORS "Treat Warnings As Errors" OFF)
-    option(myproject_ENABLE_SANITIZER_ADDRESS "Enable address sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_LEAK "Enable leak sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_UNDEFINED "Enable undefined sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_THREAD "Enable thread sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_MEMORY "Enable memory sanitizer" OFF)
-    option(myproject_ENABLE_UNITY_BUILD "Enable unity builds" OFF)
-    option(myproject_ENABLE_CLANG_TIDY "Enable clang-tidy" OFF)
-    option(myproject_ENABLE_CPPCHECK "Enable cpp-check analysis" OFF)
-    option(myproject_ENABLE_PCH "Enable precompiled headers" OFF)
-    option(myproject_ENABLE_CACHE "Enable ccache" OFF)
+  if(NOT PROJECT_IS_TOP_LEVEL OR embedded_compute_PACKAGING_MAINTAINER_MODE)
+    option(embedded_compute_ENABLE_IPO "Enable IPO/LTO" OFF)
+    option(embedded_compute_WARNINGS_AS_ERRORS "Treat Warnings As Errors" OFF)
+    option(embedded_compute_ENABLE_SANITIZER_ADDRESS "Enable address sanitizer" OFF)
+    option(embedded_compute_ENABLE_SANITIZER_LEAK "Enable leak sanitizer" OFF)
+    option(embedded_compute_ENABLE_SANITIZER_UNDEFINED "Enable undefined sanitizer" OFF)
+    option(embedded_compute_ENABLE_SANITIZER_THREAD "Enable thread sanitizer" OFF)
+    option(embedded_compute_ENABLE_SANITIZER_MEMORY "Enable memory sanitizer" OFF)
+    option(embedded_compute_ENABLE_UNITY_BUILD "Enable unity builds" OFF)
+    option(embedded_compute_ENABLE_CLANG_TIDY "Enable clang-tidy" OFF)
+    option(embedded_compute_ENABLE_CPPCHECK "Enable cpp-check analysis" OFF)
+    option(embedded_compute_ENABLE_PCH "Enable precompiled headers" OFF)
+    option(embedded_compute_ENABLE_CACHE "Enable ccache" OFF)
   else()
-    option(myproject_ENABLE_IPO "Enable IPO/LTO" ON)
-    option(myproject_WARNINGS_AS_ERRORS "Treat Warnings As Errors" ON)
-    option(myproject_ENABLE_SANITIZER_ADDRESS "Enable address sanitizer" ${SUPPORTS_ASAN})
-    option(myproject_ENABLE_SANITIZER_LEAK "Enable leak sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_UNDEFINED "Enable undefined sanitizer" ${SUPPORTS_UBSAN})
-    option(myproject_ENABLE_SANITIZER_THREAD "Enable thread sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_MEMORY "Enable memory sanitizer" OFF)
-    option(myproject_ENABLE_UNITY_BUILD "Enable unity builds" OFF)
-    option(myproject_ENABLE_CLANG_TIDY "Enable clang-tidy" ON)
-    option(myproject_ENABLE_CPPCHECK "Enable cpp-check analysis" ON)
-    option(myproject_ENABLE_PCH "Enable precompiled headers" OFF)
-    option(myproject_ENABLE_CACHE "Enable ccache" ON)
+    option(embedded_compute_ENABLE_IPO "Enable IPO/LTO" ON)
+    option(embedded_compute_WARNINGS_AS_ERRORS "Treat Warnings As Errors" ON)
+    option(embedded_compute_ENABLE_SANITIZER_ADDRESS "Enable address sanitizer" ${SUPPORTS_ASAN})
+    option(embedded_compute_ENABLE_SANITIZER_LEAK "Enable leak sanitizer" OFF)
+    option(embedded_compute_ENABLE_SANITIZER_UNDEFINED "Enable undefined sanitizer" ${SUPPORTS_UBSAN})
+    option(embedded_compute_ENABLE_SANITIZER_THREAD "Enable thread sanitizer" OFF)
+    option(embedded_compute_ENABLE_SANITIZER_MEMORY "Enable memory sanitizer" OFF)
+    option(embedded_compute_ENABLE_UNITY_BUILD "Enable unity builds" OFF)
+    option(embedded_compute_ENABLE_CLANG_TIDY "Enable clang-tidy" ON)
+    option(embedded_compute_ENABLE_CPPCHECK "Enable cpp-check analysis" ON)
+    option(embedded_compute_ENABLE_PCH "Enable precompiled headers" OFF)
+    option(embedded_compute_ENABLE_CACHE "Enable ccache" ON)
   endif()
 
   if(NOT PROJECT_IS_TOP_LEVEL)
     mark_as_advanced(
-      myproject_ENABLE_IPO
-      myproject_WARNINGS_AS_ERRORS
-      myproject_ENABLE_SANITIZER_ADDRESS
-      myproject_ENABLE_SANITIZER_LEAK
-      myproject_ENABLE_SANITIZER_UNDEFINED
-      myproject_ENABLE_SANITIZER_THREAD
-      myproject_ENABLE_SANITIZER_MEMORY
-      myproject_ENABLE_UNITY_BUILD
-      myproject_ENABLE_CLANG_TIDY
-      myproject_ENABLE_CPPCHECK
-      myproject_ENABLE_COVERAGE
-      myproject_ENABLE_PCH
-      myproject_ENABLE_CACHE)
+      embedded_compute_ENABLE_IPO
+      embedded_compute_WARNINGS_AS_ERRORS
+      embedded_compute_ENABLE_SANITIZER_ADDRESS
+      embedded_compute_ENABLE_SANITIZER_LEAK
+      embedded_compute_ENABLE_SANITIZER_UNDEFINED
+      embedded_compute_ENABLE_SANITIZER_THREAD
+      embedded_compute_ENABLE_SANITIZER_MEMORY
+      embedded_compute_ENABLE_UNITY_BUILD
+      embedded_compute_ENABLE_CLANG_TIDY
+      embedded_compute_ENABLE_CPPCHECK
+      embedded_compute_ENABLE_COVERAGE
+      embedded_compute_ENABLE_PCH
+      embedded_compute_ENABLE_CACHE)
   endif()
 
-  myproject_check_libfuzzer_support(LIBFUZZER_SUPPORTED)
-  if(LIBFUZZER_SUPPORTED AND (myproject_ENABLE_SANITIZER_ADDRESS OR myproject_ENABLE_SANITIZER_THREAD OR myproject_ENABLE_SANITIZER_UNDEFINED))
+  embedded_compute_check_libfuzzer_support(LIBFUZZER_SUPPORTED)
+  if(LIBFUZZER_SUPPORTED AND (embedded_compute_ENABLE_SANITIZER_ADDRESS OR embedded_compute_ENABLE_SANITIZER_THREAD OR embedded_compute_ENABLE_SANITIZER_UNDEFINED))
     set(DEFAULT_FUZZER ON)
   else()
     set(DEFAULT_FUZZER OFF)
   endif()
 
-  option(myproject_BUILD_FUZZ_TESTS "Enable fuzz testing executable" ${DEFAULT_FUZZER})
+  option(embedded_compute_BUILD_FUZZ_TESTS "Enable fuzz testing executable" ${DEFAULT_FUZZER})
 
 endmacro()
 
-macro(myproject_global_options)
-  if(myproject_ENABLE_IPO)
+macro(embedded_compute_global_options)
+  if(embedded_compute_ENABLE_IPO)
     include(cmake/InterproceduralOptimization.cmake)
-    myproject_enable_ipo()
+    embedded_compute_enable_ipo()
   endif()
 
-  myproject_supports_sanitizers()
+  embedded_compute_supports_sanitizers()
 
-  if(myproject_ENABLE_HARDENING AND myproject_ENABLE_GLOBAL_HARDENING)
+  if(embedded_compute_ENABLE_HARDENING AND embedded_compute_ENABLE_GLOBAL_HARDENING)
     include(cmake/Hardening.cmake)
     if(NOT SUPPORTS_UBSAN 
-       OR myproject_ENABLE_SANITIZER_UNDEFINED
-       OR myproject_ENABLE_SANITIZER_ADDRESS
-       OR myproject_ENABLE_SANITIZER_THREAD
-       OR myproject_ENABLE_SANITIZER_LEAK)
+       OR embedded_compute_ENABLE_SANITIZER_UNDEFINED
+       OR embedded_compute_ENABLE_SANITIZER_ADDRESS
+       OR embedded_compute_ENABLE_SANITIZER_THREAD
+       OR embedded_compute_ENABLE_SANITIZER_LEAK)
       set(ENABLE_UBSAN_MINIMAL_RUNTIME FALSE)
     else()
       set(ENABLE_UBSAN_MINIMAL_RUNTIME TRUE)
     endif()
-    message("${myproject_ENABLE_HARDENING} ${ENABLE_UBSAN_MINIMAL_RUNTIME} ${myproject_ENABLE_SANITIZER_UNDEFINED}")
-    myproject_enable_hardening(myproject_options ON ${ENABLE_UBSAN_MINIMAL_RUNTIME})
+    message("${embedded_compute_ENABLE_HARDENING} ${ENABLE_UBSAN_MINIMAL_RUNTIME} ${embedded_compute_ENABLE_SANITIZER_UNDEFINED}")
+    embedded_compute_enable_hardening(embedded_compute_options ON ${ENABLE_UBSAN_MINIMAL_RUNTIME})
   endif()
 endmacro()
 
-macro(myproject_local_options)
+macro(embedded_compute_local_options)
   if(PROJECT_IS_TOP_LEVEL)
     include(cmake/StandardProjectSettings.cmake)
   endif()
 
-  add_library(myproject_warnings INTERFACE)
-  add_library(myproject_options INTERFACE)
+  add_library(embedded_compute_warnings INTERFACE)
+  add_library(embedded_compute_options INTERFACE)
 
   include(cmake/CompilerWarnings.cmake)
-  myproject_set_project_warnings(
-    myproject_warnings
-    ${myproject_WARNINGS_AS_ERRORS}
+  embedded_compute_set_project_warnings(
+    embedded_compute_warnings
+    ${embedded_compute_WARNINGS_AS_ERRORS}
     ""
     ""
     ""
@@ -171,66 +171,66 @@ macro(myproject_local_options)
 
   if(NOT EMSCRIPTEN)
     include(cmake/Sanitizers.cmake)
-    myproject_enable_sanitizers(
-      myproject_options
-      ${myproject_ENABLE_SANITIZER_ADDRESS}
-      ${myproject_ENABLE_SANITIZER_LEAK}
-      ${myproject_ENABLE_SANITIZER_UNDEFINED}
-      ${myproject_ENABLE_SANITIZER_THREAD}
-      ${myproject_ENABLE_SANITIZER_MEMORY})
+    embedded_compute_enable_sanitizers(
+      embedded_compute_options
+      ${embedded_compute_ENABLE_SANITIZER_ADDRESS}
+      ${embedded_compute_ENABLE_SANITIZER_LEAK}
+      ${embedded_compute_ENABLE_SANITIZER_UNDEFINED}
+      ${embedded_compute_ENABLE_SANITIZER_THREAD}
+      ${embedded_compute_ENABLE_SANITIZER_MEMORY})
   endif()
 
-  set_target_properties(myproject_options PROPERTIES UNITY_BUILD ${myproject_ENABLE_UNITY_BUILD})
+  set_target_properties(embedded_compute_options PROPERTIES UNITY_BUILD ${embedded_compute_ENABLE_UNITY_BUILD})
 
-  if(myproject_ENABLE_PCH)
+  if(embedded_compute_ENABLE_PCH)
     target_precompile_headers(
-      myproject_options
+      embedded_compute_options
       INTERFACE
       <vector>
       <string>
       <utility>)
   endif()
 
-  if(myproject_ENABLE_CACHE)
+  if(embedded_compute_ENABLE_CACHE)
     include(cmake/Cache.cmake)
-    myproject_enable_cache()
+    embedded_compute_enable_cache()
   endif()
 
   include(cmake/StaticAnalyzers.cmake)
-  if(myproject_ENABLE_CLANG_TIDY)
-    myproject_enable_clang_tidy(myproject_options ${myproject_WARNINGS_AS_ERRORS})
+  if(embedded_compute_ENABLE_CLANG_TIDY)
+    embedded_compute_enable_clang_tidy(embedded_compute_options ${embedded_compute_WARNINGS_AS_ERRORS})
   endif()
 
-  if(myproject_ENABLE_CPPCHECK)
-    myproject_enable_cppcheck(${myproject_WARNINGS_AS_ERRORS} "" # override cppcheck options
+  if(embedded_compute_ENABLE_CPPCHECK)
+    embedded_compute_enable_cppcheck(${embedded_compute_WARNINGS_AS_ERRORS} "" # override cppcheck options
     )
   endif()
 
-  if(myproject_ENABLE_COVERAGE)
+  if(embedded_compute_ENABLE_COVERAGE)
     include(cmake/Tests.cmake)
-    myproject_enable_coverage(myproject_options)
+    embedded_compute_enable_coverage(embedded_compute_options)
   endif()
 
-  if(myproject_WARNINGS_AS_ERRORS)
+  if(embedded_compute_WARNINGS_AS_ERRORS)
     check_cxx_compiler_flag("-Wl,--fatal-warnings" LINKER_FATAL_WARNINGS)
     if(LINKER_FATAL_WARNINGS)
       # This is not working consistently, so disabling for now
-      # target_link_options(myproject_options INTERFACE -Wl,--fatal-warnings)
+      # target_link_options(embedded_compute_options INTERFACE -Wl,--fatal-warnings)
     endif()
   endif()
 
-  if(myproject_ENABLE_HARDENING AND NOT myproject_ENABLE_GLOBAL_HARDENING)
+  if(embedded_compute_ENABLE_HARDENING AND NOT embedded_compute_ENABLE_GLOBAL_HARDENING)
     include(cmake/Hardening.cmake)
     if(NOT SUPPORTS_UBSAN 
-       OR myproject_ENABLE_SANITIZER_UNDEFINED
-       OR myproject_ENABLE_SANITIZER_ADDRESS
-       OR myproject_ENABLE_SANITIZER_THREAD
-       OR myproject_ENABLE_SANITIZER_LEAK)
+       OR embedded_compute_ENABLE_SANITIZER_UNDEFINED
+       OR embedded_compute_ENABLE_SANITIZER_ADDRESS
+       OR embedded_compute_ENABLE_SANITIZER_THREAD
+       OR embedded_compute_ENABLE_SANITIZER_LEAK)
       set(ENABLE_UBSAN_MINIMAL_RUNTIME FALSE)
     else()
       set(ENABLE_UBSAN_MINIMAL_RUNTIME TRUE)
     endif()
-    myproject_enable_hardening(myproject_options OFF ${ENABLE_UBSAN_MINIMAL_RUNTIME})
+    embedded_compute_enable_hardening(embedded_compute_options OFF ${ENABLE_UBSAN_MINIMAL_RUNTIME})
   endif()
 
 endmacro()
--- cmake/Cache.cmake
+++ cmake/Cache.cmake
@@ -1,5 +1,5 @@
 # Enable cache if available
-function(myproject_enable_cache)
+function(embedded_compute_enable_cache)
   set(CACHE_OPTION
       "ccache"
       CACHE STRING "Compiler cache to be used")
